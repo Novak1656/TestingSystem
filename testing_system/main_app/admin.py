b8df20d2406ea3669b9f067bd353259492be1b6c
acclrtrrr <andrey.novikov2001@yandex.ru>
2022-11-11 23:13:00 +0300

    Создал модели таблиц БД для приложения 'main_app': 1. TestResults 2. Test 3. TestQuestions 4. TestAnswers 5. Tag 6. Category

--- testing_system/main_app/admin.py
+++ testing_system/main_app/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Category, Tag, TestAnswers, TestQuestions, Test, TestResults
 
-# Register your models here.
+
+@admin.register(Category, Tag)
+class CatTagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'slug', 'title', 'created_at']
+    list_display_links = ['id', 'slug']
+    list_filter = ['created_at']
+    search_fields = ['title']
+    save_as = True
+
+
+@admin.register(TestAnswers)
+class TestAnswersAdmin(admin.ModelAdmin):
+    list_display = ['id', 'question', 'answer', 'is_right']
+    list_display_links = ['id', 'question']
+    list_filter = ['is_right']
+    search_fields = ['answer']
+    save_as = True
+
+
+@admin.register(TestQuestions)
+class TestQuestionsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'test', 'question', 'image', 'answers_type']
+    list_display_links = ['id', 'test']
+    list_filter = ['answers_type']
+    search_fields = ['question']
+    save_as = True
+
+
+@admin.register(Test)
+class TestAdmin(admin.ModelAdmin):
+    list_display = ['slug', 'title', 'author', 'category', 'passed_times', 'is_published', 'published_at', 'created_at']
+    list_display_links = ['slug']
+    list_filter = ['author', 'category', 'is_published']
+    search_fields = ['title']
+    save_as = True
+
+
+@admin.register(TestResults)
+class TestResultsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'test', 'user', 'score', 'completed_at']
+    list_display_links = ['id', 'test']
+    list_filter = ['score']
+    search_fields = ['test', 'user']
+    save_as = True
--- testing_system/main_app/models.py
+++ testing_system/main_app/models.py
@@ -1,3 +1,222 @@
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django.db import models
+from django_unique_slugify import slugify, unique_slugify
+from unidecode import unidecode
 
-# Create your models here.
+
+def question_image_path(instance, filename):
+    return f'img/{instance.test}/questions_img/{filename}'
+
+
+class Category(models.Model):
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=255
+    )
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=255,
+        unique=True
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Добавлен',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Категория теста'
+        verbose_name_plural = 'Категории тестов'
+        ordering = ['title']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(unidecode(self.title))
+        super(Category, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=255
+    )
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=255,
+        unique=True
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Добавлен',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Тег теста'
+        verbose_name_plural = 'Теги тестов'
+        ordering = ['title']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.slug = slugify(unidecode(self.title))
+        super(Tag, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class TestAnswers(models.Model):
+    question = models.ForeignKey(
+        verbose_name='Вопрос',
+        to='TestQuestions',
+        on_delete=models.CASCADE,
+        related_name='answers'
+    )
+    answer = models.TextField(
+        verbose_name='Ответ'
+    )
+    is_right = models.BooleanField(
+        verbose_name='Правильный',
+        default=False
+    )
+
+    class Meta:
+        verbose_name = 'Ответ теста'
+        verbose_name_plural = 'Ответы тестов'
+
+    def __str__(self):
+        return f'Answer #{self.pk}'
+
+
+class TestQuestions(models.Model):
+    ANSWERS_TYPES = [('Expanded', 'Развёрнутый ответ'), ('Variable', 'С вариантами ответа')]
+
+    test = models.ForeignKey(
+        verbose_name='Тест',
+        to='Test',
+        on_delete=models.CASCADE,
+        related_name='questions'
+    )
+    question = models.TextField(
+        verbose_name='Вопрос',
+        max_length=500
+    )
+    image = models.ImageField(
+        verbose_name='Изображение',
+        upload_to=question_image_path,
+        blank=True,
+        null=True
+    )
+    answers_type = models.CharField(
+        verbose_name='Тип ответа',
+        max_length=255,
+        choices=ANSWERS_TYPES,
+        default='Variable'
+    )
+
+    class Meta:
+        verbose_name = 'Вопрос теста'
+        verbose_name_plural = 'Вопросы тестов'
+
+    def __str__(self):
+        return f'{self.test} quest: #{self.pk}'
+
+
+class Test(models.Model):
+    slug = models.SlugField(
+        verbose_name='Слаг',
+        max_length=255
+    )
+    title = models.CharField(
+        verbose_name='Название',
+        max_length=255
+    )
+    description = models.TextField(
+        verbose_name='Описание',
+        blank=True
+    )
+    author = models.ForeignKey(
+        verbose_name='Автор',
+        to='users_app.User',
+        on_delete=models.CASCADE,
+        related_name='tests'
+    )
+    category = models.ForeignKey(
+        verbose_name='Категория',
+        to=Category,
+        on_delete=models.CASCADE,
+        related_name='tests'
+    )
+    tags = models.ManyToManyField(
+        verbose_name='Теги',
+        to=Tag,
+        related_name='tests',
+        blank=True,
+    )
+    passed_times = models.PositiveIntegerField(
+        verbose_name='Раз пройдено',
+        default=0
+    )
+    is_published = models.BooleanField(
+        verbose_name='Опубликовано',
+        default=False
+    )
+    created_at = models.DateTimeField(
+        verbose_name='Дата создания',
+        auto_now_add=True
+    )
+    published_at = models.DateTimeField(
+        verbose_name='Дата публикации',
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+        ordering = ['-created_at']
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            unique_slugify(self, slugify(unidecode(self.title)))
+        super(Test, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class TestResults(models.Model):
+    test = models.ForeignKey(
+        verbose_name='Тест',
+        to=Test,
+        on_delete=models.CASCADE,
+        related_name='results'
+    )
+    user = models.ForeignKey(
+        verbose_name='Пользователь',
+        to='users_app.User',
+        on_delete=models.CASCADE,
+        related_name='results'
+    )
+    score = models.PositiveIntegerField(
+        verbose_name='Результат',
+        validators=[MaxLengthValidator(100), MinLengthValidator(0)]
+    )
+    right_answers = models.ManyToManyField(
+        verbose_name='Правильные ответы',
+        to=TestQuestions,
+        related_name='results'
+    )
+    completed_at = models.DateTimeField(
+        verbose_name='Пройден',
+        auto_now_add=True
+    )
+
+    class Meta:
+        verbose_name = 'Результат теста'
+        verbose_name_plural = 'Результаты тестов'
+        ordering = ['-completed_at']
+
+    def __str__(self):
+        return f'{self.user}: Test-{self.test}'
